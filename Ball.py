74bccaeb0776856c9229442aa053187a76cb0fe1
Zeb <zeb.santos@kearsarge.org>
2017-12-06 13:56:11 -0500

    Started on 12/6/2017

---
+++ Ball.py
@@ -0,0 +1,27 @@
+import pygame, math
+
+class Ball():
+	def __init__(self, image, speed = [0,0], 
+				 pos = [0,0], size = None):
+		 self.image = pygame.image.load(image)
+		 self.rect = self.image.get_rect(topleft = pos)
+		 self.speed = [self.speedx,
+					   self.speedy] = speed
+	def move(self):
+		self.rect.move_ip(self.speed)
+		
+	def wallBounce(self, size):
+		width = size[0]
+		height = size[1]
+		
+		if self.rect.left < 0 or self.rect.right > width:
+			self.speed[0]  = -self.speed[0]
+		if self.rect.top < 0 or self.rect.bottom > height:
+			self.speed[1]  = -self.speed[1]
+			
+			
+	def ballBounce(self, other):
+		if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
+			if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
+				self.speed[0]  = -self.speed[0]
+				self.speed[1]  = -self.speed[1]
