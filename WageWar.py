1994a68998908923a8630458ba2abfb613b85296
Zeb <zeb.santos@kearsarge.org>
2017-12-14 13:34:58 -0500

    ALL DONE BY ME

--- WageWar.py
+++ WageWar.py
@@ -10,8 +10,25 @@ screen = pygame.display.set_mode(size)
 bgColor = [r, g, b] = [138, 138, 138]
 
 balls = [Ball("Ball/fire.png", [3, 1], [0,0], [30,30])]
+balls = [Ball("Ball/it.png", [2, 4], [0,0], [15,20])]
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        
+    for ball in balls:
+        ball.move()
+        ball.wallBounce(size)
+        
+        
+            
+            
+    for ball in balls:
+        screen.blit(ball.image, ball.rect)
+    pygame.display.flip()
+    clock.tick(60)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
